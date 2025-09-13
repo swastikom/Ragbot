63d375f0d0e6532b7648ac60284aba0101e9ddb0
Swastik Dhar <swastikdhar18@gmail.com>
2025-09-13 12:35:54 +0530

    Creating Streamlit structure

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,2 @@
+
 import streamlit as st
-
-st.title("My First Streamlit App")
-st.header("Welcome!")
-st.write("This is a simple Streamlit application.")
-
-# Add a slider widget
-x = st.slider('Select a value', 0, 100)
-st.write('The selected value is', x)
-
-# Add a button
-if st.button('Click me'):
-    st.write('Button was clicked!')
